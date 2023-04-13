7b591dfdfb339c470a7ce80bc5ab62621559177a
lilgibs <lil.gibran01@gmail.com>
2023-04-14 01:05:46 +0700

    feat: FE - Integrasi register login BE, dan setup Redux

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import { useDispatch, useSelector } from "react-redux";
+import { checkLogin } from "./features/user/userSlice";
 
 function App() {
+  const dispatch = useDispatch()
+  const userToken = localStorage.getItem('user_token')
+  const userGlobal = useSelector(state => state.user.user)
+
+  useEffect(() => {
+    dispatch(checkLogin(userToken))
+  }, [])
+
   return (
     <div>
       <Routes>
--- fe/src/pages/Login.jsx
+++ fe/src/pages/Login.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-// import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../features/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  //   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch();
+  const userGlobal = useSelector((state) => state.user.user)
+  const navigate = useNavigate()
+  const userToken = localStorage.getItem("user_token");
 
   const LoginSchema = Yup.object().shape({
     email: Yup.string()
@@ -14,50 +20,68 @@ function Login() {
       .min(8, "Password Harus Lebih dari 8 karakter")
       .required("Password Tidak Boleh Kosong"),
   });
+
+  const handleLoginUser = async (values) => {
+    dispatch(loginUser(values))
+  }
+
+  useEffect(() => {
+    if (userToken) {
+      navigate("/");
+    } else {
+      setIsLoading(false);
+    }
+  }, [localStorage.getItem("user_token")]);
+
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        validationSchema={LoginSchema}
-        onSubmit={(value) => {
-          // alert("testing");
-        }}
-      >
-        {(props) => {
-          // console.log(props);
-          return (
-            <div className="w-1/3 mx-auto my-9 bg-blue-100 p-6 rounded-md">
-              <p className="text-xl font-bold text-center mb-6">Page Login</p>
-              <Form>
-                <div className="flex flex-col">
-                  <label htmlFor="email">Email</label>
-                  <Field type="text" name="email" className="border-2" />
-                  <ErrorMessage
-                    component="div"
-                    name="email"
-                    style={{ color: "red" }}
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <label htmlFor="password">Password</label>
-                  <Field type="password" name="password" className="border-2" />
-                  <ErrorMessage
-                    component="div"
-                    name="password"
-                    style={{ color: "red" }}
-                  />
-                </div>
-                <button
-                  type="submit"
-                  className="bg-blue-500 py-2 px-3 rounded-md mt-10"
-                >
-                  Submit
-                </button>
-              </Form>
-            </div>
-          );
-        }}
-      </Formik>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={LoginSchema}
+          onSubmit={(values) => {
+            handleLoginUser(values)
+          }}
+        >
+          {(props) => {
+            // console.log(props);
+            return (
+              <div className="w-1/3 mx-auto my-9 bg-blue-100 p-6 rounded-md">
+                <p className="text-xl font-bold text-center mb-6">Page Login</p>
+                <Form>
+                  <div className="flex flex-col">
+                    <label htmlFor="email">Email</label>
+                    <Field type="text" name="email" className="border-2" />
+                    <ErrorMessage
+                      component="div"
+                      name="email"
+                      style={{ color: "red" }}
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label htmlFor="password">Password</label>
+                    <Field type="password" name="password" className="border-2" />
+                    <ErrorMessage
+                      component="div"
+                      name="password"
+                      style={{ color: "red" }}
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="bg-blue-500 py-2 px-3 rounded-md mt-10"
+                  >
+                    Submit
+                  </button>
+                </Form>
+              </div>
+            );
+          }}
+        </Formik>
     </div>
   );
 }
