2349e7431d40e802609ba60de9a4033c7b813006
lilgibs <lil.gibran01@gmail.com>
2023-04-14 00:57:14 +0700

    feat: BE - Membuat authentikasi login, dan check login dengan token

--- be/app.js
+++ be/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const { db, query } = require("./config/database")
 
 const { authRoutes, productRoutes } = require('./routes');
 
