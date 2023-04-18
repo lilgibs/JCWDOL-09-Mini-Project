2723282c2a467483da0f0a2c4a0f629f780e96e9
lilgibs <lil.gibran01@gmail.com>
2023-04-18 23:21:36 +0700

    feat : FE - Landin Page

---
+++ fe/src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Filter, Navbar } from '../components'
+
+function LandingPage() {
+  return (
+    <div>
+      <Navbar/>
+      <Filter/>
+    </div>
+  )
+}
+
+export default LandingPage
