e7d5e88f95548b8634663e52419b9cbd80ee0c47
lilgibs <lil.gibran01@gmail.com>
2023-04-20 17:03:25 +0700

    update : BE - Remove filter products and merge into get all products

--- be/controllers/productController.js
+++ be/controllers/productController.js
@@ -3,51 +3,81 @@ const { db, query } = require("../config/database");
 module.exports = {
   getAllProducts: async (req, res) => {
     try {
-      const products = await query(`select * from products where active = 1`)
-      res.status(200).send({ data: products })
-    } catch (error) {
-      res.status(500).json({ message: 'Error', error });
-    }
-  },
-  getProductsFilter: async (req, res) => {
-    try {
-      const { id_category, sortBy } = req.query
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 10;
+      const offset = (page - 1) * limit;
+      const { id_category, sortBy } = req.query;
 
-      let sql = `SELECT * FROM products WHERE active = 1`
+      let sql = `select * from products where active = 1 `;
 
       if (id_category) {
         sql += ` AND id_category = ${db.escape(id_category)}`;
       }
 
       switch (sortBy) {
-        case 'price_low_to_high':
+        case "price_low_to_high":
           sql += " ORDER BY price ASC";
           break;
-        case 'price_high_to_low':
+        case "price_high_to_low":
           sql += " ORDER BY price DESC";
           break;
         default:
           break;
       }
 
+      sql += ` LIMIT ${limit} OFFSET ${offset}`;
+
       const products = await query(sql)
-      res.status(200).send({ data: products })
+
+      const count = await query(
+        `SELECT COUNT(*) as total FROM products WHERE active = 1 ${
+          id_category ? `AND id_category = ${db.escape(id_category)}` : ''
+        }`
+      );
+      const total = count[0].total;
+
+      res.status(200).send({ data: products, total });
     } catch (error) {
-      res.status(500).json({ message: 'Error', error });
+      res.status(500).json({ message: "Error", error });
     }
   },
   getAllProductsUser: async (req, res) => {
-    const id  = req.user.id
+    const id = req.user.id;
     try {
-      const products = await query(`select * from products where id_user = ${db.escape(id)}`)
-      
+      const products = await query(
+        `select * from products where id_user = ${db.escape(id)}`
+      );
+
       if (products.length === 0) {
-        res.status(200).send({ message: 'Tidak ada produk' });
+        res.status(200).send({ message: "Tidak ada produk" });
       } else {
         res.status(200).send({ data: products });
       }
     } catch (error) {
-      res.status(500).json({ message: 'Error', error });
+      res.status(500).json({ message: "Error", error });
     }
   },
-}
+  AddProduct: async (req, res) => {
+    const idUser = req.user.id;
+    const { name, price, description, id_category } = req.body;
+    const image = req.file.path;
+    try {
+      const products = await query(`
+      insert into products(id, name, price, image, description, id_category, active, id_user)
+      values(
+        null,
+        ${db.escape(name)},
+        ${db.escape(price)},
+        ${db.escape(image)},
+        ${db.escape(description)},
+        ${db.escape(id_category)},
+        1,
+        ${db.escape(idUser)}
+      )
+      `);
+      res.status(200).send({ data: products });
+    } catch (error) {
+      res.status(500).json({ message: "Error", error });
+    }
+  },
+};
--- be/routes/productRoute.js
+++ be/routes/productRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { productController } = require("../controllers");
 const { verifyToken } = require("../middleware/authMiddleware");
+const  uploadMiddleware  = require("../middleware/uploadMiddleware");
 const router = express.Router()
 
 router.get('/', productController.getAllProducts)
 router.get('/user', verifyToken, productController.getAllProductsUser)
-router.get('/search', productController.getProductsFilter)
+router.post('/', verifyToken, uploadMiddleware.single('image'), productController.AddProduct);
 
 module.exports = router
